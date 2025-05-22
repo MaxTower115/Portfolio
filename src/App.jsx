adb7932a3d3926aba0a66e991bb48d3afc734a2d
MaxTower115 <max.delatorre@outlook.com>
2025-05-22 13:20:50 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MainModeler from "./pages/MainModeler/MainModeler";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* <MainModeler /> */}
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/MainModeler/MainModeler.jsx
@@ -0,0 +1,12 @@
+import "./MainModeler.css";
+import refImg from "../../assets/ref.png";
+
+const MainModeler = () => {
+  return (
+    <div className="main-modeler">
+      <img src={refImg} alt="" />
+    </div>
+  );
+};
+
+export default MainModeler;
